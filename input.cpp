7220d86c7b393b74a2ab72793ecacbd0dea93f08
Abhishek Singh <167851013+S-Abhishekk@users.noreply.github.com>
2024-07-03 20:57:31 +0000

    outputt

---
+++ input.cpp
@@ -0,0 +1,12 @@
+//In this file Big Daddy will learn basic input output concepts//
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cout<< "Enter the values: ";
+    cin >> x >> y;
+    cout << "Value of x: " << x << " and y: " << y;
+    return 0;
+    }
