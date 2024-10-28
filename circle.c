b85243c5bc22f532dee39aa56e44c7701b0d8cd9
Abhishek Singh <167851013+S-Abhishekk@users.noreply.github.com>
2024-10-28 14:58:41 +0000

    circle hai

---
+++ circle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+
+    int radius;
+    printf("Enter the radius:\n");
+    scanf("%d", &radius);
+    printf("The area of circle is: %f", 3.14*radius*radius);
+    return 0;
+
+}
